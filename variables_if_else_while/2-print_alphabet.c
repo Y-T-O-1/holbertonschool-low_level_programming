bd86bdf1152ebfe1f2dc403b3b1e255bff94c3e7
Y-T-O-1 <7198@holbertonstudents.com>
2023-09-24 15:56:46 -0500

    added file 2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void) 
 {
-    char letter = 'a';
+	char letter = 'a';
+	
+	while (letter <= 'z') 
+{
+	putchar(letter);
+	letter++;
+}
 
-    while (letter <= 'z') {
-        putchar(letter);
-        letter++;
-    }
+	putchar('\n');
 
-    putchar('\n');
-
-    return 0;
+	return 0;
 }
