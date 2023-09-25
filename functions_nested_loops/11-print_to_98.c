0a057fd3d75f9923c006af0036da00090d303c1f
Y-T-O-1 <7198@holbertonstudents.com>
2023-09-25 16:19:06 -0500

    added file 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,29 +8,28 @@
 
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n <= 98; n++)
 		{
-			printf("%d", i);
-			if (i != 98)
-				{
-				printf(", ");
-				}
-			}
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
 		}
+		printf("\n");
+	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (; n >= 98; n--)
 		{
-			printf("%d", i);
-			{
-				printf(", ");
-			}
-		}
-	}
+			printf("%d", n);
 
-	printf("\n");
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
 }
