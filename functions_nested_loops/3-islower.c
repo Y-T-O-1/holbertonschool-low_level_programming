ca3e0082ea8feb3a6a87e95598bbc6eab279e52a
Y-T-O-1 <7198@holbertonstudents.com>
2023-09-25 15:14:18 -0500

    added file 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -14,11 +14,11 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return 1; // Character is lowercase
+		return 1; /* Character is lowercase */
 	}
 
 	else
 	{
-		return 0; // Character is not lowercase
+		return 0; /*y Character is not lowercase */
 	}
 }
