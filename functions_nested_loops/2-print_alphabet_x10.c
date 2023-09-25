7788f57460d0544e799b9b4c43235421ce6abf05
Y-T-O-1 <7198@holbertonstudents.com>
2023-09-25 14:59:28 -0500

    added file 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#includ "main.h"
+
+/**
+* print_alphabet - Check description
+*
+* Description: Prints the lowercase alphabet 10x, followed by a new line
+*
+* Return: Nothing.
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+	char j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
