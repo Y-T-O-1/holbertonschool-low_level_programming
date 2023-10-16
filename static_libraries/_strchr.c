68425384bc5c5f77dfe7809bf7524e05fa9adf9e
Y-T-O-1 <7198@holbertonstudents.com>
2023-10-16 11:35:37 -0500

    created static library

---
+++ static_libraries/_strchr.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+char *_strchr(char *s, char c) {
+    /* Implementation of _strchr function */
+    /* ... */
+}
---
+++ static_libraries/_strlen.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int _strlen(char *s)
+{
+    /* Implementation of _strlen function */
+    /* ... */
+}
