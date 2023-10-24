6f8a71dbcdcda1bb6611e723fb2035d5008c441a
Y-T-O-1 <7198@holbertonstudents.com>
2023-10-23 20:09:04 -0500

    added files

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,7 +23,8 @@ int **alloc_grid(int width, int height)
                 array[i] = (int *)malloc(sizeof(int) * width);
                 if (array[i] == NULL)
                 {
-                        for (int k = 0; k < i; k++)
+                        int k;
+                        for (k = 0; k < i; k++)
                                 free(array[k]);
                         free(array);
                         return (NULL);
