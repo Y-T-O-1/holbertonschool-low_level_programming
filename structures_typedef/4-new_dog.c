5974e14cb693d70087032c94505124f7bfe9b0a4
Y-T-O-1 <7198@holbertonstudents.com>
2023-10-30 13:36:28 -0500

    added file 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -25,7 +25,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	new_dog->name = malloc((name_len + 1) * sizeof(char));
-	if (new_dog->name == NULL) {
+	if (new_dog->name == NULL)
+	{
 		free(new_dog);
 		return (NULL);
 	}
@@ -35,7 +36,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->age = age;
 
 	new_dog->owner = malloc((owner_len + 1) * sizeof(char));
-	if (new_dog->owner == NULL) {
+	if (new_dog->owner == NULL)
+	{
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
